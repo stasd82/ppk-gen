9802dd9325cbe3ad1e83032e41f1ecfab7cdc7a3
sta <stasd82@yandex.ru>
2022-07-19 23:12:49 +0300

    Implement public key generation

--- README.md
+++ README.md
@@ -2,5 +2,7 @@
 Private and public keys generator
 
 To generate a private/public key PEM file:
+
 > openssl genpkey -algorithm RSA -out private.pem -pkeyopt rsa_keygen_bits:2048
+
 > openssl rsa -pubout -in private.pem -out public.pem
--- main.go
+++ main.go
@@ -28,7 +28,7 @@ func genKey() error {
 
 	privateFile, err := os.Create("private.pem")
 	if err != nil {
-		return err
+		return fmt.Errorf("creating private file: %w", err)
 	}
 	defer privateFile.Close()
 
@@ -40,6 +40,28 @@ func genKey() error {
 	if err := pem.Encode(privateFile, &privateBlock); err != nil {
 		return fmt.Errorf("encoding to private file: %w", err)
 	}
+
+	pkixBytes, err := x509.MarshalPKIXPublicKey(&privateKey.PublicKey)
+	if err != nil {
+		return fmt.Errorf("marshaling public key: %w", err)
+	}
+
+	publicFile, err := os.Create("public.pem")
+	if err != nil {
+		return fmt.Errorf("creating public file: %w", err)
+	}
+	defer publicFile.Close()
+
+	publicBlock := pem.Block{
+		Type:  "RSA PUBLIC KEY",
+		Bytes: pkixBytes,
+	}
+
+	if err := pem.Encode(publicFile, &publicBlock); err != nil {
+		return fmt.Errorf("encoding to public file: %w", err)
+	}
+
+	fmt.Println("private and public key files generated")
 
 	return nil
 }
